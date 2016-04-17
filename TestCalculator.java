9ad7601da44686d207684a22fbe5c382774bf25e
LiaoKunZhi <a0956823675@gmail.com>
2016-04-17 16:55:55 +0800

    Create TestCalculator.java
    New a main class and put a new main method inside to start the program.Then, put a code inside the main method to call the class Calculator.

---
+++ TestCalculator.java
@@ -0,0 +1,14 @@
+//U10416043   廖崑智
+
+import javafx.application.Application;
+
+//Main class
+public class TestCalculator {
+    
+    //Main method
+    public static void main(String[] args){
+        
+		  //Call the Calculator.class
+	  	Application.launch(Calculator.class, args);
+	}
+}
